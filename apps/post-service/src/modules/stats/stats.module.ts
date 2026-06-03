dea5fbed41ef7f2ecd8077f212c97f214b666792
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2026-06-04 00:21:24 +0700

    feat(post-stats): register StatsAdminController and StatsAdminService in StatsModule

--- apps/post-service/src/modules/stats/stats.module.ts
+++ apps/post-service/src/modules/stats/stats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ViewCronService } from './services/view-cron.service';
 import { StatsRepository } from './repositories/stats.repository';
+import { StatsAdminController } from './admin/controllers/stats.controller';
+import { StatsAdminService } from './admin/services/stats.service';
 
 @Module({
-  providers: [StatsRepository, ViewCronService],
+  controllers: [StatsAdminController],
+  providers: [StatsRepository, ViewCronService, StatsAdminService],
 })
 export class StatsModule {}
