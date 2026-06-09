36683c1c1c62898148b1f23e399b0f074f43761e
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2026-06-09 16:06:33 +0700

    thêm enum + bổ sung message group

--- apps/iam-service/src/modules/role/enums/index.ts
+++ apps/iam-service/src/modules/role/enums/index.ts
@@ -1,1 +1,2 @@
 export { BasicStatusOptions as statuses } from '../../../common/enums/basic-status.enum';
+export { RoleType, RoleTypeOptions as roleTypes } from './role-type.enum';
---
+++ apps/iam-service/src/modules/role/enums/role-type.enum.ts
@@ -0,0 +1,9 @@
+export enum RoleType {
+  system = 'system',
+  group = 'group',
+}
+
+export const RoleTypeOptions = [
+  { id: RoleType.system, name: 'System - Hệ thống' },
+  { id: RoleType.group, name: 'Group - Nhóm' },
+];
